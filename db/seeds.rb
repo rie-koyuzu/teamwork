117bfd79dc9a1b452d6e438f775d1ef940ebcb3f
EC2 Default User <ec2-user@ip-172-31-41-246.ap-northeast-1.compute.internal>
2023-03-29 05:56:53 +0000

    初期データ登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,24 @@
 
 # 店長アカウント
 Admin.create!(
+ [
 	employee_number: '100000',
-	password: 11111111,
+	password: '11111111',
 	email: 'admin@1.com',
 	last_name: '田中',
 	first_name: '太郎'
+ ]
+)
+
+Member.create!(
+ [
+  last_name: 'テスト',
+  first_name: 'メンバー',
+  last_name_eng: 'test',
+  first_name_eng: 'member',
+  employee_number: '1234',
+  affiliation: '運営部',
+  email: 'testmember@1.com',
+  password: 'testmember'
+ ]
 )
